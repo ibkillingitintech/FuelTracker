46bcba4c1012308b041c73213fc7c95d35bacc36
ibkillingitintech <ibkillingit247@gmail.com>
2025-04-28 09:17:11 -0400

    Initial Commit

---
+++ FuelTracker/FuelTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelTrackerApp.swift
+//  FuelTracker
+//
+//  Created by Ibraheem k. Rawlinson on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FuelTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
